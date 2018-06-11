4652462186e0d91c5d507505267b8a8fc68f1201
Frank Lehner <frank.lehner@online.de>
2018-06-11 11:08:06 +0200

    windows.py: Create library for window functions

---
+++ libs/windows.py
@@ -0,0 +1,27 @@
+"""
+Window functions
+"""
+
+
+class Windows(object):
+    """
+    Class Windows
+    """
+    def __init__(self, data, window):
+        """
+        Constructor
+        """
+        self.data = data
+        self.window = window
+
+    def get_window(self, column, window_type):
+        """
+        return a window for special type
+        """
+        return self.data[column].rolling(self.window, win_type=window_type)
+
+    def moving_average(self, column):
+        """
+        Calculating the moving average
+        """
+        return self.data[column].rolling(self.window).mean()
